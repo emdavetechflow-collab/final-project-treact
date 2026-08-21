0a3478ba596dbe33cd96fea62617b7492cc93708
Emmanuel Davies <emdavetechflow@gmail.com>
2026-08-21 10:09:32 -0400

    Final project  website edit

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function openMenu() {
-    document.body.classList.add("menu--open");
+  document.body.classList.add("menu--open");
 }
 
 function closeMenu() {
-    document.body.classList.remove("menu--open");
+  document.body.classList.remove("menu--open");
 }
+
+// Close menu when clicking any link or button inside menu
+const menuLinks = document.querySelectorAll(
+  ".menu__links a, .menu__links button",
+);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
